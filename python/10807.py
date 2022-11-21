84c400d12cdbdfaa0b8a0abad6fc51ebcebed8f9
eraser502 <dohun8758@naver.com>
2022-11-21 18:21:22 +0900

    delete dir

--- python/10807.py
+++
@@ -1,6 +0,0 @@
-# 개수 세기
-n = int(input())
-ns = list(map(int, input().split()))
-v = int(input())
-print(ns.count(v))
-
--- python/18108.py
+++
@@ -1,5 +0,0 @@
-for i in range(6):
- print(1-int(input()))
- print(1-int(input()))
- print(2-int(input()))
- 
